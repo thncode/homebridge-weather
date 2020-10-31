1bae05730cd1ff5f21b648564f3f40da7e6707cd
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2020-10-31 17:31:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,48 @@ module.exports = function (homebridge) {
     UUIDGen = homebridge.hap.uuid;
     FakeGatoHistoryService = require("fakegato-history")(homebridge);
 
-    homebridge.registerAccessory("homebridge-weather", "Weather", Weather);
+    homebridge.registerAccessory("homebridge-weather", "Wetter", Weather);
+};
+
+function Weather(log, config) {
+
+    var that = this;
+    this.log = log;
+    this.name = config.name;
+    this.displayName = this.name;
+    this.deviceId = config.deviceId;
+
+    this.config = config;
+
+    this.setUpServices();
+};
+
+Weather.prototype.setUpServices = function () {
+
+    this.informationService = new Service.AccessoryInformation();
+
+    this.informationService
+        .setCharacteristic(Characteristic.Manufacturer, "Thomas Nemec")
+        .setCharacteristic(Characteristic.Model, "Wetter")
+        .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name)
+    this.informationService.getCharacteristic(Characteristic.FirmwareRevision)
+        .on('get', this.getFirmwareRevision.bind(this));
+        
+    this.tempService = new Service.TemperatureSensor("Temperatur");
+    this.tempService.getCharacteristic(Characteristic.CurrentTemperature)
+		.setProps({minValue: -50})
+        .on('get', this.getCurrentTemperature.bind(this));
+    this.tempService.getCharacteristic(Characteristic.StatusLowBattery)
+        .on('get', this.getStatusLowBattery.bind(this));
+    this.tempService.getCharacteristic(Characteristic.StatusActive)
+        .on('get', this.getStatusActive.bind(this));
+
+    this.fakeGatoHistoryService = new FakeGatoHistoryService("weather", this, { storage: 'fs' });
+};
+
+Weather.prototype.getServices = function () {
+    var services = [this.informationService, 
+    				this.tempService, 
+					this.fakeGatoHistoryService];
+    return services;
 };
